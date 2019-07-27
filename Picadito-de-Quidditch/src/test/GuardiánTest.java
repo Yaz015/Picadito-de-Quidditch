d6c8a76ff56ec63d7a0312001f473c079238e9e4
Ana <analubari@gmail.com>
2019-07-26 22:16:49 -0300

    agrego test(?

---
+++ Picadito-de-Quidditch/src/test/GuardiánTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import main.Guardián;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuardiánTest {
+    Guardián guardián;
+}
