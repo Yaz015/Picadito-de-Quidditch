5e1d170eb282c207b7831deca9113b6ca6398989
Ana <analubari@gmail.com>
2019-07-29 15:22:12 -0300

    arreglo skill, porcentaje de salud en escoba y añosdesdefabricacion

---
+++ Picadito-de-Quidditch/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+    public static void main(String[] args){
+
+        SaetaDeFuego saeta = new SaetaDeFuego();
+
+        Cazador cazador1 = new Cazador(23, 34, 54, 80, saeta);
+    }
+}
