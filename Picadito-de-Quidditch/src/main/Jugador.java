7448053a788b00ab64399a8f988c765d70fe4db0
Yaz <yaz_01hotamil.com>
2019-07-29 22:26:24 -0300

    Algunos métodos

--- Picadito-de-Quidditch/src/main/Jugador.java
+++ Picadito-de-Quidditch/src/main/Jugador.java
@@ -25,7 +25,17 @@ public abstract class Jugador {
     public Integer velocidadDeEscoba() {
         return this.escobaDelJugador().velocidadEscoba();
     }
+    public Boolean lePasaElTrapo(Jugador jugador){ return jugador.habilidad()<=this.habilidad()/2;}
 
+    /**Si un jugador es groso, que se cumple si su habilidad es mayor al promedio de su
+ equipo y su velocidad mayor a un valor arbitrario(KEEEEEEEEJAJA) que a medida que el mercado de
+ escobas mejora se actualiza para todos por igual.**/
+
+    public Boolean esGroso() { return
+            this.habilidad() > jugadores.stream()
+                    .map( j -> j.habilidad()).reduce( 0, Integer::sum );
+
+    }}
 
 }
 
