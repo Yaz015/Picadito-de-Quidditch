0e27c6ba0a2437b3841964ca7030be439b76dba4
Ana <analubari@gmail.com>
2019-08-03 20:21:58 -0300

    exception no hay jugador falta test

---
+++ Picadito-de-Quidditch/src/main/java/exceptions/NoHayJugadoresEnEquipoException.java
@@ -0,0 +1,6 @@
+package main.java.exceptions;
+
+public class NoHayJugadoresEnEquipoException extends RuntimeException {
+    public NoHayJugadoresEnEquipoException(String no_hay_jugadores) {
+    }
+}
