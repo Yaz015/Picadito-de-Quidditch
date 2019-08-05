cca74eec9de0e6e534c7118ac1dafc45933edf1c
Ana <analubari@gmail.com>
2019-08-05 02:06:59 -0300

    3-1 y 4-1.a

--- Picadito-de-Quidditch/src/main/java/Guardián.java
+++ Picadito-de-Quidditch/src/main/java/Guardián.java
@@ -1,16 +1,34 @@
 package main.java;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Guardián extends Jugador {
     private Integer nivelDeReflejos;
     private Integer fuerza;
+    private Integer randomNumber = 3;
 
-    public Guardián(Integer skill, Integer nivelDeReflejos, Integer fuerza, Integer peso, Escoba escoba, Equipo equipo){
+
+    public Guardián(Integer skill, Integer nivelDeReflejos, Integer fuerza, Integer peso, Escoba escoba, Equipo equipo) {
         super(peso, escoba, skill, equipo);
-        this.nivelDeReflejos=nivelDeReflejos;
-        this.fuerza=fuerza;
+        this.nivelDeReflejos = nivelDeReflejos;
+        this.fuerza = fuerza;
     }
 
-    public Integer habilidad(){
-        return super.habilidad()+this.nivelDeReflejos+this.fuerza;
+    public Integer habilidad() {
+        return super.habilidad() + this.nivelDeReflejos + this.fuerza;
     }
+
+    public Boolean puedeBloquear(Jugador jugador) {
+     return 3 = getRandomNumber();
+    }
+
+    public Integer getRandomNumber(){
+        Random ran=new Random();
+     return this.randomNumber=  (int)(ran.nextDouble()*3)+1;
+    }
+
+
 }
+
---
+++ Picadito-de-Quidditch/src/main/java/Pelota.java
@@ -0,0 +1,5 @@
+package main.java;
+
+public interface Pelota {
+    String getTipo();
+}
