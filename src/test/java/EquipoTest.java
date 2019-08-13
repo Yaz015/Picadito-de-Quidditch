5f35f241bebf93de70f8a491222dec54d8ccd452
Ana <analubari@gmail.com>
2019-08-12 21:18:20 -0300

    mockito

--- src/test/java/EquipoTest.java
+++ src/test/java/EquipoTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
