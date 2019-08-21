15d0b38e42c18a9430767f52a093958a3bc1d334
vickybilla <jimenezvictoria@hotmail.com>
2019-08-21 20:54:07 -0300

    intentando subida

--- Picadito-de-Quidditch/src/main/Campo.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class Campo {
-    private Pelota quaffle;
-    private Pelota snitch;
-    private Pelota bludger;
-
-}
--- Picadito-de-Quidditch/src/main/Escoba.java
+++
@@ -1,6 +0,0 @@
-package main;
-
-public interface Escoba {
-    Integer velocidadEscoba();
-    void escobaGolpeada();
-}
